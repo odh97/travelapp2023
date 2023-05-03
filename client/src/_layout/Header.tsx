8ee2d658ff0829bbc265db218b629a32f9441342
Oh Daehyeon <xzdheogus1@gmail.com>
2023-05-03 19:07:50 +0900

    v1.0.4 header css 작업

--- client/src/_layout/Header.tsx
+++ client/src/_layout/Header.tsx
@@ -1,13 +1,27 @@
 import React from 'react'
 import '../styles/layout/Header.scss'
 
+// icon
+import { CiUser } from "react-icons/ci";
+
+
 function Header(): JSX.Element{
   return (
     <header>
       <div className='header-inner'>
-        <div className='brand-Name'>Logo</div>
-        <div>nav</div>
-        <div>user</div>
+        <div className='brand-Name'><h1><a href="javascript:void(0)">travel</a></h1></div>
+        <nav>
+          <div className='nav-inner'>
+            <a href="javascript:void(0)">게시판<span></span></a>
+            <a href="javascript:void(0)">item<span></span></a>
+            <a href="javascript:void(0)">item<span></span></a>
+            <a href="javascript:void(0)">item<span></span></a>
+          </div>
+        </nav>
+        <div className='user'>
+          <div className='user-icon'><CiUser/></div>
+          <span className='user-name'>user</span>
+        </div>
       </div>
     </header>
   )
