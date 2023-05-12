2840f102b3fcdbc0314acfec44b09e1a8f90ba76
Oh Daehyeon <xzdheogus1@gmail.com>
2023-05-12 20:15:19 +0900

    v1.0.9 chat 기능 구현중
    오늘 내가 예외처리한 부분 때운에 state 변경될때 HTML에 반영이 안되는 현상 해결했다...

--- server/index.js
+++ server/index.js
@@ -8,6 +8,16 @@ app.use(express.json());
 var cors = require('cors');
 app.use(cors());
 
+// openai API
+const { Configuration, OpenAIApi } = require("openai");
+const configuration = new Configuration({ apiKey: "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",  /* 배포 전 재발급 */ });
+const openai = new OpenAIApi(configuration);
+
+// papago API
+var client_id = 'tyDodzGry78dFOOdgyA6'; // 배포 전 재발급
+var client_secret = 'Efl5YyRqwk'; // 배포 전 재발급
+var api_url = 'https://openapi.naver.com/v1/papago/n2mt';
+
 // server open
 app.listen(8080, function () {
   console.log('listening on 8080');
@@ -18,8 +28,57 @@ app.listen(8080, function () {
   // 응답.sendFile(path.join(__dirname, '/../client/build/index.html'));
 // })
 
+let test_DB_data = [
+  `user: 일본 2박3일 여행 일정 만들어줘`,
+  `AI: 일본 2박3일 여행 일정을 만들어드리겠습니다! 하지만 일본은 매우 다양한 관광지와 맛집이 많아서, 여행 목적과 선호하는 스타일에 따라 다양한 일정이 가능합니다. 따라서, 가능한 자세한 여행 계획을 알려주시면 보다 구체적인 일정을 제시할 수 있습니다. 아래는 대략적인 일정 예시입니다.
+  
+  일정:
+  Day 1:
+  
+  도쿄 도착
+  아사쿠사 신사 방문
+  우에노 동물원 공원에서 동물들 관람
+  스시점에서 일식 즐기기
+  Day 2:
+  
+  후지산 일일 관광 투어
+  다케시타 고텐바 전망대에서 후지산 전망 감상
+  오도아라케 시장에서 재미있는 쇼핑 즐기기
+  신주쿠에서 레스토랑에서 저녁 식사
+  Day 3:
+  
+  아침에 아마노하시드메 도보관광
+  하라주쿠 지역에서 쇼핑 및 레스토랑 즐기기
+  도쿄에서 국제센터에서 쇼핑 즐기기
+  도쿄 출발
+  이 일정은 일본의 대표적인 도시인 도쿄와 후지산 등 일본에서 유명한 관광지를 방문하는 것을 기반으로 한 일정입니다. 여행 목적, 예산, 선호하는 음식 등을 고려하여 일정을 조정할 수 있습니다.`,
+  `user: 오사카가 가고 싶은데 오사카 여행 일정으로 만들어 줄래?`,
+  `AI: 물론입니다! 오사카 여행 일정을 제안해드리겠습니다.
+  
+  일정:
+  Day 1:
+  
+  오사카 도착 후 호텔 체크인
+  오사카성 탐방
+  도톤보리 지역에서 유명한 음식점에서 오사카의 로컬 음식 체험
+  신세카이에서 타워의 전망을 즐기고 지역의 가게들을 탐험
+  Day 2:
+  
+  유니버설 스튜디오 재팬(USJ) 방문
+  해리 포터의 마법 세계와 슈퍼 히어로들을 만나는 등 다양한 어트랙션 체험
+  교토로 이동
+  키요미즈데라 사원 방문
+  기온 거리에서 전통 가게들을 탐험하며 저녁 식사
+  Day 3:
+  
+  오사카 수상버스 투어로 오사카의 유명한 명소들을 감상
+  신오사카 스카이빌딩에서 오사카 도시의 아름다운 전망 감상
+  오사카에서의 마지막 저녁 식사를 즐긴 후 공항으로 이동
+  이 일정은 오사카의 주요 명소인 오사카성, 도톤보리, 신세카이를 방문하며, 유니버설 스튜디오 재팬(USJ)을 경험하고 교토의 키요미즈데라 사원을 감상하는 것을 중점으로 한 일정입니다. 자유로운 시간을 가지고 현지의 다양한 음식과 문화를 즐기는 것도 추천드립니다. 여행 일정은 개인의 선호와 시기에 따라 변경될 수 있으니 참고 바랍니다. 즐거운 오사카 여행이 되길 바랍니다!`,
+  ];
+
 app.get('/', function(요청, 응답){
-  응답.send("확인용");
+  응답.json({test_DB_data : test_DB_data});
 })
 
 // 데이터 조회 해보기
@@ -36,23 +95,15 @@ app.post('/testData', function(요청, 응답){
 
 
 
-// openai API
-const { Configuration, OpenAIApi } = require("openai");
-const configuration = new Configuration({ apiKey: "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",  /* 배포 전 재발급 */ });
-const openai = new OpenAIApi(configuration);
-
-// papago API
-var client_id = 'tyDodzGry78dFOOdgyA6'; // 배포 전 재발급
-var client_secret = 'Efl5YyRqwk'; // 배포 전 재발급
-var api_url = 'https://openapi.naver.com/v1/papago/n2mt';
-var papago_query_data = "잉글랜드 2박 3일 여행 추천해줘";
 
 
 
 
 
-app.post('/chatTest', function (req, res) {
-  console.log("chat Test start");
+// 채팅 기능 구현
+app.post('/chatEnter', function (req, res) {
+  let papago_query_data = req.body.userValue;
+  console.log("chat Enter start");
 
   let options1 = {
     url: api_url,
@@ -95,7 +146,7 @@ app.post('/chatTest', function (req, res) {
             console.log("en ==> ko translation 성공");
             console.log(result2);
 
-            res.send(result2);
+            res.json({data : result2});
 
           } else {
             res.status(response.statusCode).end();
@@ -115,63 +166,6 @@ app.post('/chatTest', function (req, res) {
   });
 
 });
-
-
-
-
-
-
-
-
-// 채팅 기능 구현
-app.get('/chatEnter', function(요청, 응답){
-
-  // openai
-  const { Configuration, OpenAIApi } = require("openai");
-  const configuration = new Configuration({
-    apiKey: "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",  // 배포 전 재발급
-  });
-  const openai = new OpenAIApi(configuration);
-
-  openai.createCompletion({
-    model: "text-davinci-003",
-    prompt: `
-            \n\nQ: What is human life expectancy in the United States?
-            \nA: Human life expectancy in the United States is 78 years.
-            \n\nQ: 여행지 추천해줄 수 있어?
-            \nA:  여러분이 즐길 수 있는 여행지를 추천해 드리겠습니다. 가족과 함께 다녀볼 수 있는 곳으로, 
-            뉴욕에 가면 미국 사람들이 많이
-            \n\nQ: continue
-            \nA: 방문하는 주요 관광지로는 십일탑과 뉴욕 중심부에 있는 빅뱅과 아마존 스페이스에 놀러갈 수 있습니다. 또한, 미국 선진화
-            \n\nQ: continue
-            \nA: 의 기원을 볼 수 있는 미국 역사 요새인 피닉스빌과 레이커 베이, 유네스코 세계 문화 유산으로 등재된 기틀란드 대학교까지 여
-            \n\nQ: continue
-            \nA: 러들이 즐길 수 있는 장소는 많습니다. 이외에도 메이지 인디애나, 라스베이거스, 밴쿠버, 워쉽턴과 같은 여러 도시를 방문해
-            \n\nQ: continue
-            \nA: 주변 자연을 눈길을 쏟을 수 있습니다. 방학 때 그리고 휴가 때 미국을 방문하실 때는 이런 여행지를 참고하세요.
-            \n\nQ: Please make a trip to China for 2 nights and 3 days
-            `,
-    temperature: 0,
-    max_tokens: 100,
-    top_p: 1,
-    frequency_penalty: 0,
-    presence_penalty: 0,
-    stop: ["\n"],
-    temperature: 0.9,
-    max_tokens: 150,
-    top_p: 1,
-    frequency_penalty: 0,
-    presence_penalty: 0.6,
-    stop: [" Human:", " AI:"],
-  }).then((result)=>{
-    console.log("============성공============");
-    console.log(result.data.choices[0].text);
-  }).catch((error)=>{
-    console.log("==============실패====================");
-    console.log(error);
-  })
-
-})
 
 
 // React Router (항상 최하단으로)
