d4de6d0577ac29395377beacbb15ca1072074604
Oh Daehyeon <xzdheogus1@gmail.com>
2023-05-16 23:00:13 +0900

    chat 기능 구현 완료 ChatRoom css 수정중

--- client/src/routes/ChatRoom.tsx
+++ client/src/routes/ChatRoom.tsx
@@ -1,4 +1,4 @@
-import React, {useRef, useState, useEffect, useCallback, useMemo} from 'react';
+import React, {useRef, useState, useEffect} from 'react';
 import '../styles/components/ChatRoom.scss'
 
 // components import
@@ -16,98 +16,101 @@ type chatObjDB = {
   [key:string] : string[],
 };
 
+// window type 추적
+declare global {
+  interface Window {
+    a: string,
+    chatDBHistory: chatObjDB,
+  }
+}
+window.a = "a";
+
 
 
 function ChatRoom(): JSX.Element{
   let [chatDBHistory, setChatDBHistory] = useState<chatObjDB>({});
 
-// 마운트
-// 대화 내용 데이터 조회
-useEffect(() => {
-  // ajax 요청
-  axios.get('http://localhost:8080/')
-  .then((result)=>{
-    let copy:chatObjDB = result.data.test_DB_data;
-    setChatDBHistory(copy);
-    console.log(copy);
-  })
-  .catch((error)=>{console.log(error)})
-}, []);
+  // 마운트
+  // 대화 내용 데이터 조회
+  useEffect(() => {
+    // ajax 요청
+    axios.get('http://localhost:8080/')
+    .then((result)=>{
+      let copy:chatObjDB = result.data.test_DB_data;
+      setChatDBHistory(copy);
+    })
+    .catch((error)=>{console.log(error)})
+  }, []);
 
-// alert 컴포넌트
-let [AlertClick, setAlertClick] = useState(false);
+  // alert 컴포넌트
+  let [AlertClick, setAlertClick] = useState(false);
 
-function handleAlert(){setAlertClick(false);}
+  function handleAlert(){setAlertClick(false);}
 
-// 채팅 인풋 줄바꿈 기능
-const textRef = useRef<HTMLTextAreaElement>(null);
-const divRef = useRef<HTMLDivElement>(null);
+  // chatRoom scroll 높이 조작
+  const chatRoomDivRef = useRef<HTMLDivElement>(null);
+  if(chatRoomDivRef.current) chatRoomDivRef.current.scrollTop + 30;
 
-let textInput = ():void =>{
-  textRef.current!.style.height = 'auto';
-  divRef.current!.style.height = 'auto';
-  textRef.current!.style.height = textRef.current!.scrollHeight + "px";
-  divRef.current!.style.height = (textRef.current!.scrollHeight+30) + "px";
-}
+  // 채팅 인풋 줄바꿈 기능
+  const inputTextRef = useRef<HTMLTextAreaElement>(null);
+  const inputDivRef = useRef<HTMLDivElement>(null);
+
+  let textInput = ():void =>{
+    inputTextRef.current!.style.height = 'auto';
+    inputDivRef.current!.style.height = 'auto';
+    inputTextRef.current!.style.height = inputTextRef.current!.scrollHeight + "px";
+    inputDivRef.current!.style.height = (inputTextRef.current!.scrollHeight+30) + "px";
+  }
 
   // chatDBHistory 업데이트
   useEffect(() => {
     console.log(chatDBHistory);
   }, [chatDBHistory]);
 
-const updateChatHistory = (newMessage:string)=>{
-  const copy = chatDBHistory;
-  copy.ko_chat_arr.push(newMessage);
-  setChatDBHistory(copy)
-}
+  const updateChatHistory = (newMessage:string)=>{
+    const copy = chatDBHistory;
+    copy.ko_chat_arr.push(newMessage);
+    setChatDBHistory(copy)
+  }
 
 
 
-/* 채팅 기능 */
-let [chatInputValue, setChatInputValue]= useState(``);
+  /* 채팅 기능 */
+  let [chatInputValue, setChatInputValue]= useState(``);
 
-function chatBtnFn(event: React.MouseEvent<HTMLButtonElement> | React.KeyboardEvent<HTMLTextAreaElement>){
-  console.log("chatBtnFn 실행 체크");
-  // 예외처리
-  event.preventDefault();
-  if(!chatInputValue.trim() === true) return setAlertClick(true); // 빈값과 스페이스 값만을 전송했을 경우
+  function chatBtnFn(event: React.MouseEvent<HTMLButtonElement> | React.KeyboardEvent<HTMLTextAreaElement>){
+    console.log("chatBtnFn 실행 체크");
+    // 예외처리
+    event.preventDefault();
+    if(!chatInputValue.trim() === true) return setAlertClick(true); // 빈값과 스페이스 값만을 전송했을 경우
 
-  //POST data
-  updateChatHistory(`user: ${chatInputValue.trim()}`);
-  const chatDBHistory_copy = chatDBHistory;
-  const postData = {
-    userValue: `user: ${chatInputValue.trim()}`,
-    chatDBHistory: chatDBHistory_copy,
-  };
+    //POST data
+    updateChatHistory(`user: ${chatInputValue.trim()}`);
+    const chatDBHistory_copy = chatDBHistory;
+    const postData = {
+      userValue: `user: ${chatInputValue.trim()}`,
+      chatDBHistory: chatDBHistory_copy,
+    };
 
+    // ajax 요청 진행
+    axios.post('http://localhost:8080/chatEnter', postData)
+    .then((result)=>{
+      console.log(result.data.DB_chat_data);
 
+      // data 가공
+      setChatDBHistory(result.data.DB_chat_data);
 
-  // ajax 요청 진행
-  axios.post('http://localhost:8080/chatEnter', postData)
-  .then((result)=>{
-    console.log(result.data);
+      // input 값 초기화
+      setChatInputValue(``);
+      inputTextRef.current!.style.height = '30px';
+      inputDivRef.current!.style.height = '60px';
+    })
+    .catch((error)=>{
+      console.log(error);
+    });
 
-    // data 가공
-    
-
-    // input 값 초기화
-    setChatInputValue(``);
-    textRef.current!.style.height = '30px';
-    divRef.current!.style.height = '60px';
-  })
-  .catch((error)=>{
-    console.log(error);
-  });
-
-}
-/* //채팅 기능 */
-
-
-
-
-
-
-
+  }
+  /* //채팅 기능 */
 
 
 
@@ -117,7 +120,7 @@ function chatBtnFn(event: React.MouseEvent<HTMLButtonElement> | React.KeyboardEv
     <Header />
     <main>
       <ChatList />
-      <div className='chat-room'>
+      <div className='chat-room' ref={chatRoomDivRef}>
         <div className='txt-box'>
           <ul>
           {
@@ -146,8 +149,8 @@ function chatBtnFn(event: React.MouseEvent<HTMLButtonElement> | React.KeyboardEv
           <div className='empty-box'></div>
         </div>
         <div className='chat-input-box'>
-          <div className='chat-input-box-inner' ref={divRef}>
-            <textarea cols={85} rows={1} ref={textRef}
+          <div className='chat-input-box-inner' ref={inputDivRef}>
+            <textarea cols={85} rows={1} ref={inputTextRef}
               value={chatInputValue}
               placeholder='여행 일정을 짜달라고 해보세요!'
               onChange={(e)=>{
--- server/index.js
+++ server/index.js
@@ -11,6 +11,7 @@ app.use(cors());
 
 // openai API
 const { Configuration, OpenAIApi } = require("openai");
+const { setTimeout } = require('timers/promises');
 const configuration = new Configuration({ apiKey: "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",  /* 배포 전 재발급 */ });
 const openai = new OpenAIApi(configuration);
 
@@ -32,19 +33,35 @@ app.listen(8080, function () {
 const test_DB_data2 = {
   ko_chat_arr: [
     'user: 너는 이제부터 여행컨설턴트야 관광지, 맛집, 유용한 정보 등등 여행에 관해서 좋은 정보를 알려줬으면 좋겠어',
-    'AI:물론이죠! 기꺼이 돕겠습니다. 관광지부터 시작합시다. 세계에서 가장 인기 있는 관광 명소로는 프랑스 파리의 에펠탑, 인도 아그라의 타지마할, 중국의 만리장성, 이탈리아 로마의 콜로세움이 있습니다. \n' +
-      '\n' +
-      '식당에 관해서라면, 전 세계에서 선택할 수 있는 수많은 선택지가 있다. 가장 높은 평가를 받은 식당으로는 스페인 지로나의 엘 셀레르 드 칸 로카, 프랑스 리옹의 라메르 브레이져, 일본 도쿄의 스키야바시 지로, 이탈리아 모데나의 오스테리아 프란체스카나 등이 있다. \n' +
-      '\n' +
-      '이 정보가 도움이 되었으면 좋겠어요!'
+    'AI: 물론이죠! 기꺼이 돕겠습니다. 관광지부터 시작합시다. 세계에서 가장 인기 있는 관광 명소로는 프랑스 파리의 에펠탑, 인도 아그라의 타지마할, 중국의 만리장성, 이탈리아 로마의 콜로세움이 있습니다. \n' +
+    '\n' +
+    '식당에 관해서라면, 전 세계에서 선택할 수 있는 수많은 선택지가 있다. 가장 높은 평가를 받은 식당으로는 스페인 지로나의 엘 셀레르 드 칸 로카, 프랑스 리옹의 라메르 브레이져, 일본 도쿄의 스키야바시 지로, 이탈리아 모데나의 오스테리아 프란체스카나 등이 있다. \n' +
+    '\n' +
+    '이 정보가 도움이 되었으면 좋겠어요!',
+    'user: 너는 누구지??',
+    'AI: 저는 당신의 여행 문의를 돕기 위해 고안된 AI 챗봇입니다. 관광지, 음식점 등 유용한 여행 정보를 알려드리려고 왔습니다.',
+    'user: 다음부터는 너를 여행 컨설턴트로 소개해줬으면 좋겠어',
+    'AI: 물론입니다! 지금부터 저를 여행 컨설턴트로 소개하겠습니다. 저는 당신의 모든 여행에 필요한 것들을 도우러 왔습니다. 관광지, 음식점 등 여행과 관련하여 궁금한 점이 있으면 알려주시기 바랍니다.',
+    'user: 너는 누구지?',
+    'AI: 안녕하세요! 저는 AI 챗봇이고 여행 컨설턴트입니다. 저는 당신의 모든 여행에 필요한 것들을 도우러 왔습니다. 관광지, 음식점 등 여행과 관련하여 궁금한 점이 있으면 알려주시기 바랍니다.',
   ],
   en_chat_arr: [
     'user: You are a travel consultant from now on. I want you to give me good information about travel, such as tourist attractions, restaurants, and useful information',
-    "AI:Sure thing! I'm happy to help. Let's start with tourist attractions. Some of the most popular tourist attractions in the world include the Eiffel Tower in Paris, France; the Taj Mahal in Agra, India; the Great Wall of China; and the Colosseum in Rome, Italy. \n" +
-      '\n' +
-      'When it comes to restaurants, there are countless options to choose from around the world. Some of the most highly rated restaurants include El Celler de Can Roca in Girona, Spain; La Mère Brazier in Lyon, France; Sukiyabashi Jiro in Tokyo, Japan; and Osteria Francescana in Modena, Italy. \n' +
-      '\n' +
-      'I hope this information is helpful!'
+    "AI: Sure thing! I'm happy to help. Let's start with tourist attractions. Some of the most popular tourist attractions in the world include the Eiffel Tower in Paris, France; the Taj Mahal in Agra, India; the Great Wall of China; and the Colosseum in Rome, Italy. \n" +
+    '\n' +
+    'When it comes to restaurants, there are countless options to choose from around the world. Some of the most highly rated restaurants include El Celler de Can Roca in Girona, Spain; La Mère Brazier in Lyon, France; Sukiyabashi Jiro in Tokyo, Japan; and Osteria Francescana in Modena, Italy. \n' +
+    '\n' +
+    'I hope this information is helpful!',
+    "user:  Who are you?",
+    `AI:  I'm an AI chatbot designed to help you with travel information. I can provide you with tourist attractions, restaurants, and other useful information.`,
+    "user:  What did I tell you to do when I introduced you earlier?",
+    "AI:  When you introduced me earlier, you asked me to give you good information about travel, such as tourist attractions, restaurants, and useful information.",
+    "user:  What did I tell you to do when I introduced you?",
+    "AI:  When you introduced me, you asked me to give you good information about travel, such as tourist attractions, restaurants, and useful information.",
+    "user:  I thought you told me to introduce you as a travel consultant",
+    "AI:  Yes, that's right. You asked me to introduce myself as a travel consultant and provide you with good information about travel, such as tourist attractions, restaurants, and useful information.",
+    "user:  Then introduce yourself again",
+    "AI:  Hi there! My name is AI Chatbot and I'm a travel consultant. I'm here to help you with all your travel needs. Let me know if you have any questions about tourist attractions, restaurants, or anything else related to travel.",
   ]
 }
 
@@ -97,19 +114,25 @@ async function papagoAPI(sourcePram, targetPram, message){
     throw error;
   }
 }
-async function chatGptAPI(messageArr) {
+async function chatGptAPI(pram) {
   console.log("GPT API in");
-  console.log(messageArr);
+  console.log(pram);
+  
+  let message = "";
+  for(let item of pram){
+    message += item;
+  }
+
   try {
     const result = await openai.createCompletion({
       model: "text-davinci-003",
-      prompt: messageArr,
+      prompt: message,
       temperature: 0.9,
-      max_tokens: 2000,
+      max_tokens: 1000,
       top_p: 0.7,
       frequency_penalty: 0,
       presence_penalty: 0.6,
-      stop: [" Human:", " AI:"],
+      stop: [" user: ", " AI: "],
     });
 
     return result.data.choices[0].text;
@@ -130,13 +153,14 @@ app.post('/chatEnter', async function (req, res) {
 
   try{
     // API 통신
+/*
     const result1 = await papagoAPI("ko", "en", req.body.userValue);
     DB_chat_Obj_Data.en_chat_arr.push(result1);
     console.log("첫번째 번역 API 작업이 끝났습니다.");
     
     
     const result2 = await chatGptAPI(DB_chat_Obj_Data.en_chat_arr);
-    DB_chat_Obj_Data.en_chat_arr.push("AI: "+result2);
+    DB_chat_Obj_Data.en_chat_arr.push(result2);
     console.log(result2);
     console.log("Chat GPT API 작업이 끝났습니다.");
     
@@ -144,11 +168,20 @@ app.post('/chatEnter', async function (req, res) {
     DB_chat_Obj_Data.ko_chat_arr.push(result3);
     console.log("마지막 번역 API 작업이 끝났습니다.");
     console.log(DB_chat_Obj_Data);
+*/
 
     // DB 저장
     // 미작성
 
-    res.json({data : DB_chat_Obj_Data})
+    console.log("aaaaaaaaaaaaaaaaaaaaaa");
+    let number = 1;
+    number++;
+    DB_chat_Obj_Data.en_chat_arr.push("user: test Data string"+number+number+number+number);
+    DB_chat_Obj_Data.en_chat_arr.push("AI: test Data string"+number+number+number+number);
+    DB_chat_Obj_Data.ko_chat_arr.push("AI: AI 테스트 데이터 문자"+number+number+number+number);
+    console.log(DB_chat_Obj_Data);
+      res.json({DB_chat_data : DB_chat_Obj_Data})
+
   }
   catch(error){
     console.log(error);
